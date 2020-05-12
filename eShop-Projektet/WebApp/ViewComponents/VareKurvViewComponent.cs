6362bdd73dc8ec272ee352a399a9bb01ae8fe5b5
Amalie Blanco Martens <amal5434@elevcampus.dk>
2020-05-12 08:46:51 +0200

    Viewcomponent Varekurv start

---
+++ eShop-Projektet/WebApp/ViewComponents/VareKurvViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ServiceLayer.ProduktService;
+using ServiceLayer.ProduktService.Concrete;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.ViewComponents
+{
+    public class VareKurvViewComponent : ViewComponent
+    {
+        private readonly IProduktService _produktService;
+
+
+        public VareKurvViewComponent(IProduktService produktService)
+        {
+            _produktService = produktService;
+
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
